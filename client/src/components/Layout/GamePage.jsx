c984252824e4bc30743018524003bf41647790b3
Shalini <shalinityagi64@gmail.com>
2019-03-22 11:10:00 +0530

    feat: Add modal component

--- client/src/components/Layout/GamePage.jsx
+++ client/src/components/Layout/GamePage.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 import ScoreCard from '../UI/ScoreCard';
+import Modal from '../UI/Modal';
 
 const styles = theme => ({
   root: {
@@ -15,37 +16,49 @@ const styles = theme => ({
   },
 });
 
-const GamePage = (props) => {
-  const {
-    render, classes, startGame, score, status,
-  } = props;
-  return (
-    <React.Fragment>
-      <Paper className={classes.root} elevation={1}>
-        <Grid container spacing={16}>
-          <Grid item xs={3} />
-          <Grid item xs={6}>
-            <Button variant="outlined" size="medium" color="primary" onClick={startGame}>
-              Start
-            </Button>
-            <div>
-              Score:
-              {score}
-            </div>
-            <div>
-              Status:
-              {status === 2 ? 'GAME OVER' : status}
-            </div>
-            {render()}
+class GamePage extends Component {
+  state = {
+    openModal: false,
+  }
+
+  componentDidMount() {
+    this.setState({ openModal: true });
+  }
+
+  render() {
+    const {
+      render, classes, startGame, score, status,
+    } = this.props;
+    const { openModal } = this.state;
+    return (
+      <React.Fragment>
+        <Paper className={classes.root} elevation={1}>
+          <Modal open={openModal} />
+          <Grid container spacing={16}>
+            <Grid item xs={3} />
+            <Grid item xs={6}>
+              <Button variant="outlined" size="medium" color="primary" onClick={startGame}>
+                Start
+              </Button>
+              <div>
+                Score:
+                {score}
+              </div>
+              <div>
+                Status:
+                {status === 2 ? 'GAME OVER' : status}
+              </div>
+              {render()}
+            </Grid>
+            <Grid item xs={3} sm container>
+              <ScoreCard />
+            </Grid>
           </Grid>
-          <Grid item xs={3} sm container>
-            <ScoreCard />
-          </Grid>
-        </Grid>
-      </Paper>
-    </React.Fragment>
-  );
-};
+        </Paper>
+      </React.Fragment>
+    );
+  }
+}
 
 GamePage.propTypes = {
   score: PropTypes.number.isRequired,
---
+++ client/src/components/UI/Modal.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+
+const styles = theme => ({
+  paper: {
+    position: 'absolute',
+    width: theme.spacing.unit * 50,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing.unit * 4,
+    outline: 'none',
+    top: '40%',
+    left: '30%',
+  },
+});
+
+class SimpleModal extends React.Component {
+  render() {
+    const { classes, open } = this.props;
+
+    return (
+      <div>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={open}
+          onClose={this.handleClose}
+        >
+          <div className={classes.paper}>
+            <Typography variant="subtitle1" id="simple-modal-description">
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </div>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+SimpleModal.propTypes = {
+  classes: PropTypes.shape().isRequired,
+  open: PropTypes.bool.isRequired,
+};
+
+export default withStyles(styles)(SimpleModal);
